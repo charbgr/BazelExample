6340bd4eff0cb9b7ef6ff983d714228ac470ae3d
charbgr <bmpak04@gmail.com>
2017-12-15 16:21:00 +0200

    test incremental build

--- android/src/main/java/com/google/bazel/example/android/activities/MainActivity.java
+++ android/src/main/java/com/google/bazel/example/android/activities/MainActivity.java
@@ -14,25 +14,10 @@
 
 package com.google.bazel.example.android.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,85 +25,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-  }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    if (id == R.id.action_ping) {
-      new AsyncTask<String, Void, String>() {
-        public static final int READ_TIMEOUT_MS = 5000;
-        public static final int CONNECTION_TIMEOUT_MS = 2000;
-
-        private String inputStreamToString(InputStream stream) throws IOException {
-          StringBuilder result = new StringBuilder();
-          try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
-            String line;
-            while ((line = reader.readLine()) != null) {
-              result.append(line);
-            }
-          } finally {
-            stream.close();
-          }
-          return result.toString();
-        }
-
-        private HttpURLConnection getConnection(String url) throws IOException {
-          URLConnection urlConnection = new URL(url).openConnection();
-          urlConnection.setConnectTimeout(CONNECTION_TIMEOUT_MS);
-          urlConnection.setReadTimeout(READ_TIMEOUT_MS);
-          return (HttpURLConnection) urlConnection;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-          String url = params[0];
-          HttpURLConnection connection = null;
-          try {
-            connection = getConnection(url);
-            return new JSONObject(inputStreamToString(connection.getInputStream()))
-                .getString("requested");
-          } catch (IOException e) {
-            Log.e("background", "IOException", e);
-            return null;
-          } catch (JSONException e) {
-            Log.e("background", "JSONException", e);
-            return null;
-          } finally {
-            if (connection != null) {
-              connection.disconnect();
-            }
-          }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-          TextView textView = (TextView) findViewById(R.id.text_view);
-          if (result == null) {
-            Toast.makeText(
-                    MainActivity.this, getString(R.string.error_sending_request), Toast.LENGTH_LONG)
-                .show();
-            textView.setText("???");
-            return;
-          }
-          textView.setText(result);
-        }
-      }.execute("http://10.0.2.2:8080/boop");
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+    LinearLayout foo = (LinearLayout) findViewById(R.id.foo);
+    TextView v = new TextView(this);
+    v.setText("test");
+    foo.addView(v);
   }
 }
